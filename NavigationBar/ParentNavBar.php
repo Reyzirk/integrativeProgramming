6352e8df0464a3b8d5304c9342b898448cb3847d
Oon Kheng Huang <oonkh-pm19@student.tarc.edu.my>
2022-03-06 13:03:47 +0800

    Add Parent Navigation Bar V1

---
+++ NavigationBar/ParentNavBar.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<!--
+============================================
+Copyright 2022 Omega International Junior School. All Right Reserved.
+Web Application is under GNU General Public License v3.0
+============================================
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        
+         <!-- Favicons -->
+        <link href="../css/NavBarCSS/assets/img/favicon.png" rel="icon">
+        <link href="../css/NavBarCSS/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+        <!-- Vendor CSS Files -->
+        <link href="../css/NavBarCSS/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+        <link href="../css/NavBarCSS/assets/vendor/aos/aos.css" rel="stylesheet">
+        <link href="../css/NavBarCSS/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="../css/NavBarCSS/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+        <link href="../css/NavBarCSS/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+        <link href="../css/NavBarCSS/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+        <link href="../css/NavBarCSS/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="../css/NavBarCSS/assets/css/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        
+         <!-- ======= Header ======= -->
+        <header id="header" class="fixed-top">
+            <div class="container d-flex align-items-center">
+
+                <h1 class="logo me-auto"><a href="">OMEGA</a></h1>
+
+                <nav id="navbar" class="navbar order-last order-lg-0">
+                    <ul>
+                        <li><a href="#">Announcement</a></li>
+                        <li class="dropdown"><a href="#"><span>Course</span> <i class="bi bi-chevron-down"></i></a>
+                            <ul>
+                                <li><a href="#">View Examination</a></li>
+                                <li><a href="#">Timetable</a></li>
+                                <li><a href="#">Homework</a></li> <!--Optional, may delete it-->
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#"><span>Child</span> <i class="bi bi-chevron-down"></i></a>
+                            <ul>
+                                <li><a href="#">View Child Details</a></li>
+                                <li><a href="#">Update Child Details</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown"><a href="#"><span>Attendance</span> <i class="bi bi-chevron-down"></i></a>
+                            <ul>
+                                <li><a href="#">Child Attendance</a></li>
+                                <li class="dropdown"><a href="#"><span>COVID-19</span> <i class="bi bi-chevron-right"></i></a>
+                                    <ul>
+                                        <li><a href="#">Temperature</a></li>
+                                        <li><a href="#">Mask</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+                        <li><a href="">About</a></li>
+                        <li><a>|</a></li>
+                        <li class="dropdown"><a href="#"><span>Profile</span> <i class="bi bi-chevron-down"></i></a>
+                            <ul>
+                                <li><a href="#">My Account</a></li>
+                                <li><a href="#">Log Out</a></li>
+                            </ul>
+                        </li>
+
+                    </ul>
+
+                    <i class="bi bi-list mobile-nav-toggle"></i>
+
+                </nav> <!-- NavBar-->
+
+            </div>
+        </header> <!--End Header-->
+    </body>
+</html>
