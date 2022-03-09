9d7adb4be378403522deab9e3c40cf84273b36d4
Oon Kheng Huang <oonkh-pm19@student.tarc.edu.my>
2022-03-09 17:12:03 +0800

    Add Parent Navigation Bar V2.1

--- Components/headmeta.php
+++ Components/headmeta.php
@@ -31,10 +31,6 @@ echo("
         ");
 ?>
 
-<!-- Favicons -->
-<link href="../css/NavBarCSS/assets/img/favicon.png" rel="icon">
-<link href="../css/NavBarCSS/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
