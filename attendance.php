e977b004fd539f07a7e03f6a001404a1d3f6cc7f
NgKarKai <clhskkng6771t9@gmail.com>
2022-03-11 18:26:34 +0800

    Merge origin/master
    Conflicts:
    	Components/headmeta.php
    	NavigationBar/ParentNavBar.php
    	api.php

---
+++ attendance.php
@@ -0,0 +1,25 @@
+<?php
+include 'Function/load.php';
+?>
+<!DOCTYPE html>
+<!--
+============================================
+Copyright 2022 Omega International Junior School. All Right Reserved.
+Web Application is under GNU General Public License v3.0
+============================================
+-->
+<html>
+    <head>
+        <?php
+        include 'Components/headmeta.php';
+        ?>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        include 'NavigationBar/ParentNavBar.php';
+        ?>
+        
+    </body>
+</html>
