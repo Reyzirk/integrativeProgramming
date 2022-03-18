aa332c86f0da5c0acdce6e737157886bc12afb18
ChooMeng <pohchoomeng01@gmail.com>
2022-03-18 15:50:13 +0800

    Fix Navbar

---
+++ Database/ChildDB.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * ============================================
+ * Copyright 2022 Omega International Junior School. All Right Reserved.
+ * Web Application is under GNU General Public License v3.0
+ * ============================================
+ */
+
+/**
+ * Description of ChildDB
+ *
+ * @author Choo Meng
+ */
+require_once str_replace("InstructorArea", "", dirname(__DIR__)) . '/Database/DBController.php';
+require_once str_replace("InstructorArea", "", dirname(__DIR__)) . '/Database/MySQLQueryBuilder.php';
+class ChildDB {
+    private $instance;
+    public function __construct(){
+        $this->instance = DBController::getInstance();
+    }
+    public function validID($id): bool{
+        $builder = new MySQLQueryBuilder();
+        $query = $builder->select(array("child"), array("*"))
+                ->where("ChildID", \CustomSQLEnum::BIND_QUESTIONMARK)
+                ->query();
+        $stmt = $this->instance->con->prepare($query);
+        $stmt->bindParam(1, $id, PDO::PARAM_STR);
+        $stmt->execute();
+        $totalrows = $stmt->rowCount();
+        if ($totalrows==0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+}
